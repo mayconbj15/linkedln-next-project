25131810202b6ae6e71875203b8c25df2de4a10d
Maycon Bruno de Jesus <26802716+mayconbj15@users.noreply.github.com>
2025-02-10 14:28:10 -0300

    feat: Updates cart products

--- src/app/api/users/[id]/cart/route.ts
+++ src/app/api/users/[id]/cart/route.ts
@@ -34,3 +34,22 @@ export async function GET(request: NextRequest, { params }: {params: Params}) {
         }
     })
 }
+
+type CartBody = { productId: string }
+
+export async function POST(request: NextRequest, { params }: {params: Params}) {
+    const userId = params.id
+    const body: CartBody = await request.json()
+    const productId = body.productId
+    console.log(productId)
+    console.log(carts[userId])
+    carts[userId] = carts[userId] ? carts[userId].concat(productId) : [productId]
+    const cartProducts = carts[userId].map(id => products.find(p => p.id === id))
+
+    return new Response(JSON.stringify(cartProducts),{
+        status: 201,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
